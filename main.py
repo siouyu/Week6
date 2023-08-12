7ea975601b0d2569d455818177494f1ba9a90ec5
Siouyu Hsu <46859822+siouyu@users.noreply.github.com>
2023-08-13 00:57:58 +0800

    final

--- main.py
+++ main.py
@@ -28,11 +28,14 @@ def signup():
     name = request.form.get("name")
     username = request.form.get("username")
     password = request.form.get("password")
+    # print("名字" + name)
+    # print("帳號在這裡" + username)
+    # print("密碼在這裡" + password)
     check = "SELECT username FROM member WHERE username = %s"
     user = (username,) # Tuple，只有一個變數逗號還是要留著
     cursor.execute(check, user)
-    password = cursor.fetchone()
-    if password:
+    check_username = cursor.fetchone()
+    if check_username != None : 
         return redirect("/error?message=帳號已經被註冊")
     else:
         insert = "INSERT INTO member (name, username, password) VALUES (%s, %s, %s)"
@@ -43,7 +46,7 @@ def signup():
         select_value = (username,)
         cursor.execute(select, select_value)
         for i in cursor:
-            session["userid"] = i[0]
+            session["username"] = i[0]
         return redirect("/member")
 
 # 登入
@@ -51,12 +54,13 @@ def signup():
 def signin():
     username = request.form.get("username")
     password = request.form.get("password")
-    check = "SELECT id, password FROM member WHERE username = %s"
+    check = "SELECT password FROM member WHERE username = %s"
     check_value = (username,)
     cursor.execute(check, check_value)
-    for member in cursor:
-        if (member[0],password):
-            session["userid"] = member[1]
+    for member in cursor: 
+        # print("for 回圈裡的", member)
+        if member[0] == password:
+            session["username"] = username
             return redirect("/member")
     return redirect("/error?message=帳號或密碼輸入錯誤")
 
@@ -69,19 +73,21 @@ def signout():
 # 會員頁
 @app.route("/member")
 def member():
+    # print("哈囉")
     if session.get("username"): 
-        check_name = "SELECT name FROM member where id =%s"
-        check_value = (session["userid"],)
+        check_name = "SELECT name FROM member WHERE username =%s"
+        check_value = (session["username"],)
         cursor.execute(check_name, check_value)
         for name in cursor:
             name = name[0]
         # 留言
         content = "SELECT name, content FROM member INNER JOIN message ON member.id=message.member_id ORDER BY message.time DESC"
         cursor.execute(content)
-        i = []
+        row = []
         for i in cursor:
-            i.append([i[0],i[1]])
-        return render_template("member.html", name = name, i = i)
+            row.append([i[0],i[1]])
+        # print("row 在這裡",row)
+        return render_template("member.html", name = name, row = row) # 前面的 row 對 html，後面的 row 是 list
     else:
         return redirect("/")
 
@@ -89,8 +95,13 @@ def member():
 @app.route("/createMessage", methods=["POST"])
 def createMessage():
     message = request.form.get("message")
-    insert = "INSERT INTO message (member_id, conten) VALUES (%s, %s) "
-    insert_value = (session["userid"],message)
+    select = "SELECT id FROM member WHERE username = %s"
+    select_id = (session["username"],)
+    cursor.execute(select, select_id)
+    for id in cursor:
+        id = id[0]
+    insert = "INSERT INTO message (member_id, content) VALUES (%s, %s) "
+    insert_value = (id, message)
     cursor.execute(insert, insert_value)
     con.commit()
     return redirect("/member")
